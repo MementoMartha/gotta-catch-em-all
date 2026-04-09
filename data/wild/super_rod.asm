c054a6df92d7f0614dd8014df6c1e2bed34b9d25
Martha Schilling <harleyrayquaza@gmail.com>
2026-04-09 10:40:00 +0100

    Super Rod rework
    This changes the Super Rod to function how it does in Yellow.

--- data/wild/super_rod.asm
+++ data/wild/super_rod.asm
@@ -1,101 +1,33 @@
-; super rod encounters
-SuperRodData:
-	; map, fishing group
-	dbw PALLET_TOWN,         .Group1
-	dbw VIRIDIAN_CITY,       .Group1
-	dbw CERULEAN_CITY,       .Group3
-	dbw VERMILION_CITY,      .Group4
-	dbw CELADON_CITY,        .Group5
-	dbw FUCHSIA_CITY,        .Group10
-	dbw CINNABAR_ISLAND,     .Group8
-	dbw ROUTE_4,             .Group3
-	dbw ROUTE_6,             .Group4
-	dbw ROUTE_10,            .Group5
-	dbw ROUTE_11,            .Group4
-	dbw ROUTE_12,            .Group7
-	dbw ROUTE_13,            .Group7
-	dbw ROUTE_17,            .Group7
-	dbw ROUTE_18,            .Group7
-	dbw ROUTE_19,            .Group8
-	dbw ROUTE_20,            .Group8
-	dbw ROUTE_21,            .Group8
-	dbw ROUTE_22,            .Group2
-	dbw ROUTE_23,            .Group9
-	dbw ROUTE_24,            .Group3
-	dbw ROUTE_25,            .Group3
-	dbw CERULEAN_GYM,        .Group3
-	dbw VERMILION_DOCK,      .Group4
-	dbw SEAFOAM_ISLANDS_B3F, .Group8
-	dbw SEAFOAM_ISLANDS_B4F, .Group8
-	dbw SAFARI_ZONE_EAST,    .Group6
-	dbw SAFARI_ZONE_NORTH,   .Group6
-	dbw SAFARI_ZONE_WEST,    .Group6
-	dbw SAFARI_ZONE_CENTER,  .Group6
-	dbw CERULEAN_CAVE_2F,    .Group9
-	dbw CERULEAN_CAVE_B1F,   .Group9
-	dbw CERULEAN_CAVE_1F,    .Group9
+SuperRodSlots::
+	db PALLET_TOWN,         MAGIKARP, 4, GOLDEEN, 4, MAGIKARP, 4, GOLDEEN, 4
+	db VIRIDIAN_CITY,       MAGIKARP, 6, GOLDEEN, 6, MAGIKARP, 6, GOLDEEN, 6
+	db ROUTE_22,            MAGIKARP, 6, GOLDEEN, 6, MAGIKARP, 6, GOLDEEN, 6
+	db ROUTE_2,             MAGIKARP, 6, GOLDEEN, 6, MAGIKARP, 6, PSYDUCK, 6
+	db CERULEAN_CITY,       MAGIKARP, 12, GOLDEEN, 12, POLIWAG, 12, PSYDUCK, 12
+	db ROUTE_24,            MAGIKARP, 14, GOLDEEN, 14, POLIWAG, 14, PSYDUCK, 14
+	db ROUTE_25,            MAGIKARP, 14, GOLDEEN, 14, POLIWAG, 14, PSYDUCK, 14
+	db ROUTE_6,             MAGIKARP, 16, GOLDEEN, 16, POLIWAG, 16, PSYDUCK, 16
+	db VERMILION_CITY,      MAGIKARP, 16, TENTACOOL, 16, HORSEA, 16, TENTACOOL, 16
+	db VERMILION_DOCK,      MAGIKARP, 16, TENTACOOL, 16, HORSEA, 16, TENTACOOL, 16
+	db ROUTE_11,            MAGIKARP, 16, TENTACOOL, 16, HORSEA, 16, TENTACOOL, 16
+	db ROUTE_10,            POLIWAG, 18, MAGIKARP, 18, KRABBY, 18, SLOWPOKE, 18
+	db ROUTE_12,            POLIWAG, 20, MAGIKARP, 20, KRABBY, 20, SLOWPOKE, 20
+	db CELADON_CITY,        MAGIKARP, 22, MAGIKARP, 22, GRIMER, 22, MAGIKARP, 22
+	db ROUTE_13,            POLIWAG, 20, MAGIKARP, 20, KRABBY, 20, SLOWPOKE, 20
+	db ROUTE_17,            TENTACOOL, 26, KRABBY, 26, HORSEA, 26, POLIWHIRL, 26
+	db ROUTE_18,            TENTACOOL, 26, KRABBY, 26, HORSEA, 26, POLIWHIRL, 26
+	db ROUTE_23,            TENTACOOL, 28, KRABBY, 28, HORSEA, 28, POLIWHIRL, 28
+	db FUCHSIA_CITY,        TENTACOOL, 28, KRABBY, 28, HORSEA, 28, POLIWHIRL, 28
+	db SAFARI_ZONE_CENTER,  SLOWPOKE, 30, KRABBY, 30, PSYDUCK, 30, DRATINI, 20
+	db SAFARI_ZONE_EAST,    SLOWPOKE, 30, KRABBY, 30, PSYDUCK, 30, DRATINI, 20
+	db SAFARI_ZONE_NORTH,   SLOWPOKE, 30, KRABBY, 30, PSYDUCK, 30, DRATINI, 20
+	db SAFARI_ZONE_WEST,    SLOWPOKE, 30, KRABBY, 30, PSYDUCK, 30, DRATINI, 20
+	db ROUTE_19,            GOLDEEN, 28, HORSEA, 28, SHELLDER, 28, STARYU, 28
+	db ROUTE_20,            GOLDEEN, 32, HORSEA, 32, SHELLDER, 32, STARYU, 32
+	db ROUTE_21,            GOLDEEN, 32, HORSEA, 32, SHELLDER, 32, STARYU, 32
+	db CINNABAR_ISLAND,     GOLDEEN, 32, HORSEA, 32, SHELLDER, 32, STARYU, 32
+	db SEAFOAM_ISLANDS_B3F, GOLDEEN, 34, HORSEA, 34, SHELLDER, 34, STARYU, 34
+	db SEAFOAM_ISLANDS_B4F, GOLDEEN, 34, HORSEA, 34, SHELLDER, 34, STARYU, 34
+	db CERULEAN_CAVE_1F,    SEAKING, 52, SLOWBRO, 52, SEADRA, 52, GYARADOS, 52
+	db CERULEAN_CAVE_B1F,   SEAKING, 56, SLOWBRO, 56, SEADRA, 56, GYARADOS, 56
 	db -1 ; end
-
-; fishing groups
-; number of monsters, followed by level/monster pairs
-
-.Group1:
-	db 2
-	db 15, TENTACOOL
-	db 15, POLIWAG
-
-.Group2:
-	db 2
-	db 15, GOLDEEN
-	db 15, POLIWAG
-
-.Group3:
-	db 3
-	db 15, PSYDUCK
-	db 15, GOLDEEN
-	db 15, KRABBY
-
-.Group4:
-	db 2
-	db 15, KRABBY
-	db 15, SHELLDER
-
-.Group5:
-	db 2
-	db 23, POLIWHIRL
-	db 15, SLOWPOKE
-
-.Group6:
-	db 4
-	db 15, DRATINI
-	db 15, KRABBY
-	db 15, PSYDUCK
-	db 15, SLOWPOKE
-
-.Group7:
-	db 4
-	db 5, TENTACOOL
-	db 15, KRABBY
-	db 15, GOLDEEN
-	db 15, MAGIKARP
-
-.Group8:
-	db 4
-	db 15, STARYU
-	db 15, HORSEA
-	db 15, SHELLDER
-	db 15, GOLDEEN
-
-.Group9:
-	db 4
-	db 23, SLOWBRO
-	db 23, SEAKING
-	db 23, KINGLER
-	db 23, SEADRA
-
-.Group10:
-	db 4
-	db 23, SEAKING
-	db 15, KRABBY
-	db 15, GOLDEEN
-	db 15, MAGIKARP
